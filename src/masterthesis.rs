77c5cedcf865827ffb23dd078a3050e5885de986
Thibaud Labat <thibaud>
2025-02-09 19:29:11 +0100

    haybale: moved everything I could to masterthesis repo

--- src/masterthesis.rs
+++ src/masterthesis.rs
@@ -133,64 +133,7 @@ pub fn get_operand_symbol_or_unknown<B: Backend>(state: &State<B>, op: &Operand,
     }
 }
 
-pub fn hasNoUnknown(val: &RecordedValue) -> bool {
-    match val{
-        RecordedValue::DebugString(_) => {true}
-        RecordedValue::Unknown(_) => {false}
-        RecordedValue::Apply(x, _) => { hasNoUnknown(x)}
-        RecordedValue::Constant(_) => {true}
-        RecordedValue::Global(_) => {true}
-        RecordedValue::Deref(x) => { hasNoUnknown(x)}
-        RecordedValue::FieldAccess(a, b, c, d) => {
-            hasNoUnknown(a) && d.iter().all(|x| hasNoUnknown(x.deref()))
-        }
-        RecordedValue::BaseArgument(_, _, _) => {true}
-        RecordedValue::BinaryOperation(a, b, _) => {
-            hasNoUnknown(a) && hasNoUnknown(b)
-        }
-        RecordedValue::ICmp(a, b, _) => {
-            hasNoUnknown(a) && hasNoUnknown(b)
-        }
-        RecordedValue::Function(func) => {true}
-        RecordedValue::FunctionPointer(func) => {
-            hasNoUnknown(func)
-        }
-        RecordedValue::UnevaluatedFunctionReturnValue(_) => {
-            // TODO: should be false..?
-            true
-        }
-    }
-}
 
-
-pub fn comesFromBaseArgument(val: &RecordedValue) -> bool {
-    match val{
-        RecordedValue::DebugString(_) => {false}
-        RecordedValue::Unknown(_) => {false}
-        RecordedValue::Apply(x, _) => {comesFromBaseArgument(x)}
-        RecordedValue::Constant(_) => {false}
-        RecordedValue::Global(_) => {false}
-        RecordedValue::Deref(x) => {comesFromBaseArgument(x)}
-        RecordedValue::FieldAccess(a, b, c, d) => {
-            comesFromBaseArgument(a) || d.iter().all(|x| comesFromBaseArgument(x.deref()))
-        }
-        RecordedValue::BaseArgument(_, _, _) => {true}
-        RecordedValue::BinaryOperation(a, b, _) => {
-            comesFromBaseArgument(a) || comesFromBaseArgument(b)
-        }
-        RecordedValue::ICmp(a,b, _) => {
-            comesFromBaseArgument(a) || comesFromBaseArgument(b)
-        }
-        RecordedValue::Function(func) => {false}
-        RecordedValue::FunctionPointer(func) => {
-            comesFromBaseArgument(func)
-        }
-        RecordedValue::UnevaluatedFunctionReturnValue(_) => {
-            // TODO: we dont' know...
-            false
-        }
-    }
-}
 
 pub fn binaryOpToString(bop: &BinaryOp) -> String{
     match bop{
